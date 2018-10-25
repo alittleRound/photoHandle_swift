113fa4be072be95bf52e21aba12f3271deb1c403
wyt1996 <ibewhour@163.com>
2018-10-25 09:05:51 +0800

    Initial Commit

---
+++ photoHandle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoHandle
+//
+//  Created by tingz on 2018/10/25.
+//  Copyright © 2018 tingz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
